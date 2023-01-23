3fb921c3de2dee311d33e2c0765db2b22bb0c93d
MuhammadAnwarIbrahim <91143815+MuhammadAnwarIbrahim@users.noreply.github.com>
2023-01-23 22:17:20 +0700

    edit

--- application/views/user/atasuser.php
+++ application/views/user/atasuser.php
@@ -6,9 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>MYROKET OFFICsIAL SITE</title>
+    <title>DESA BANDAR DALAM</title>
     <!-- Favicon-->
-    <link rel="icon" type="image/x-icon" href=<?= base_url('assets/favicon.ico') ?> />
+    <link rel="icon" type="image/x-icon" href=<?= base_url('assets/logo.ico') ?> />
     <!-- Font Awesome icons (free version)-->
     <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
     <!-- Google fonts-->
@@ -30,7 +30,7 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
-                    <li class="nav-item"><a class="nav-link" href=<?= base_url('user/home') ?>>Beranda</a></li>
+                    <li class="nav-item"><a class="nav-link" href=<?= base_url('user/home') ?>>BeraEnda</a></li>
                     <li class="nav-item"><a class="nav-link" href=<?= base_url('user/tentang') ?>>Tentang</a></li>
                     <li class="nav-item "><a class="nav-link" href=<?= base_url('user') ?>>Jadi Mitra</li>
                     <li class="nav-item"><a class="nav-link" href=<?= base_url('SearchController') ?>>Cek Ongkir</a></li>
